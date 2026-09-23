f3f32d0dca157f69aae522c3d8f4431c90e4115d
Harsh <h4rshshrivastava@gmail.com>
2026-09-23 13:50:22 +0530

    project page responsiveness

--- src/Components/Views/Projects.js
+++ src/Components/Views/Projects.js
@@ -1,8 +1,9 @@
 import gsap from "gsap";
-import { useEffect, useRef, useState, useTransition } from "react";
+import { useContext, useEffect, useRef, useState, useTransition } from "react";
 import { ScrambleTextPlugin } from "gsap/all";
 import {Project_List as projects, generateGithubCardURL, generateGithubCardURLZoomed} from "./View_Components/CLASS_PROJECT";
 import loading_gif from "../Backdrop_Components/assets/loading.gif"
+import { browserContext } from "../../App";
 
 gsap.registerPlugin(ScrambleTextPlugin);
 
@@ -11,7 +12,7 @@ export default function Projects() {
     const [startFlag, setStartFlag] = useState(false);
     const [expandedProject, setExpandedProject] = useState(null);
     const [pendingCardFetch, setCardFetch] = useTransition();
-
+    const browser = useContext(browserContext)
     const ref = {
         project_header: useRef(),
         zoomed_details: useRef()
@@ -52,20 +53,15 @@ export default function Projects() {
                 project.addEventListener("mouseleave", () => {
                     gsap.to(project, {scale: 1, backgroundColor: "var(--accent-color)", duration: 0.3, ease: "power4.out"});
                 });
-                project.addEventListener("click", () => {
-                    if(expandedProject == index) {
-                        setExpandedProject(null);
-                    } else {
-                        setExpandedProject(index);
-                    }
+            });
+            if(browser.DPI != "MOBILE") {
+                document.getElementById("project_details").addEventListener("mouseenter", () => {
+                    gsap.to("#project_details", {scale: 0.95, duration: 0.3, ease: "power4.out"});
                 });
-            });
-            document.getElementById("project_details").addEventListener("mouseenter", () => {
-                gsap.to("#project_details", {scale: 0.95, duration: 0.3, ease: "power4.out"});
-            });
-            document.getElementById("project_details").addEventListener("mouseleave", () => {
-                gsap.to("#project_details", {scale: 1, duration: 0.3, ease: "power4.out"});
-            });
+                document.getElementById("project_details").addEventListener("mouseleave", () => {
+                    gsap.to("#project_details", {scale: 1, duration: 0.3, ease: "power4.out"});
+                });
+            }
         }, 200);
     },[startFlag]);
     useEffect(() => {
@@ -77,45 +73,92 @@ export default function Projects() {
         setCardFetch(async ()=>{
             await gsap.to("#project_details", {x:-100, opacity: 0, duration: 0.5, ease: "power4.out"})
             await fetch(generateGithubCardURL(projects[expandedProject].repository)).then(()=>{document.getElementById("project_details").style.backgroundImage = `url(${generateGithubCardURL(projects[expandedProject].repository)})`})
-            await fetch(generateGithubCardURLZoomed(projects[expandedProject].repository)).then(()=>{ref.zoomed_details.current.style.backgroundImage = `url(${generateGithubCardURLZoomed(projects[expandedProject].repository)})`})
+            if(browser.DPI != "MOBILE" && browser.DPI != "LDPI") {
+                await fetch(generateGithubCardURLZoomed(projects[expandedProject].repository)).then(()=>{ref.zoomed_details.current.style.backgroundImage = `url(${generateGithubCardURLZoomed(projects[expandedProject].repository)})`})
+            }
             gsap.to("#project_details", {x:0, opacity: 1, duration: 0.5, ease: "power4.out"});
         })
     }, [expandedProject]);
     return (
-        <div id="project_wrapper" style={{height:"90%", width:"100%", marginTop:"5rem"}}>
+        <div id="project_wrapper" style={{height:"90%", width:"100%", marginTop:browser.DPI === "MOBILE" ? "0rem" : "7rem"}}>
             <div className="container mx-auto my-auto" id="project_container" style={{opacity:0, overflow:"hidden", position:"relative", backgroundColor:"var(--primary-color)", outline:"0.5rem solid var(--secondary-color)", border:"0.5rem solid var(--accent-color)", outlineOffset:"-1rem", color:"var(--secondary-color)", borderRadius:"2rem", color:"var(--accent-color)"}}>
                 <div className="row mx-auto" style={{width:"100%", position:"absolute"}}>
                     <div className="col my-1">
                         <p className="display-3 megrim-regular" ref={ref.project_header} style={{textAlign:"end", marginRight:"1rem"}}></p>
                     </div>
                 </div>
-                <div className="row mx-auto" style={{width:"100%", position:"absolute", top:"5rem"}}>
+                <div className="row mx-auto" style={{width:"100%", position:"absolute", top:"4rem"}}>
                     <div className="col">
                         <p className="megrim-regular header-description" style={{textAlign:"end", marginRight:"1.5rem", opacity:0}}>This section is under construction</p>
                     </div>
                 </div>
-                <div className="row mx-auto" style={{overflow:"hidden", height:"70%", top:"8rem", position:"relative"}}>
-                    <div className="col-3 ms-3 px-4" style={{overflowY:"scroll", height:"100%", zIndex:1}}>
-                        <div className="row g-2 my-2">
-                            {projects.map((project, index) => (
-                            <div key={index} className="card mb-2 project_item">
-                                <div className="card-body" >
-                                    <h5 className="card-title project-item-name">{pendingCardFetch && expandedProject==index?"Loading..":project.name}</h5>
+                { browser.DPI != "MOBILE" && browser.DPI != "LDPI" ?
+                    <div className="row mx-auto" style={{overflow:"hidden", height:"70%", top:"8rem", position:"relative"}}>
+                        <div className="col-3 ms-3 px-4" style={{overflowY:"scroll", height:"100%", zIndex:1}}>
+                            <div className="row g-2 my-2">
+                                {projects.map((project, index) => (
+                                <div key={index} className="card mb-2 project_item"
+                                    onClick={
+                                        ()=> {
+                                            if(expandedProject == index) {
+                                                setExpandedProject(null);
+                                            } else {
+                                                setExpandedProject(index);
+                                            }
+                                        }
+                                    }>
+                                    <div className="card-body" >
+                                        <h5 className="card-title project-item-name">{pendingCardFetch && expandedProject==index?"Loading..":project.name}</h5>
+                                    </div>
                                 </div>
+                            ))}
                             </div>
-                        ))}
+                            <div className="position-absolute h-25 bottom-0 start-0 ms-4" style={{background:"linear-gradient(transparent, var(--primary-color))", width:"100%", pointerEvents:"none"}}></div>
+                            <div className="position-absolute h-25 top-0 start-0 ms-4" style={{background:"linear-gradient(var(--primary-color), transparent)", width:"100%", pointerEvents:"none"}}></div>
                         </div>
-                        <div className="position-absolute h-25 bottom-0 start-0 ms-4" style={{background:"linear-gradient(transparent, var(--primary-color))", width:"49%", pointerEvents:"none"}}></div>
-                        <div className="position-absolute h-25 top-0 start-0 ms-4" style={{background:"linear-gradient(var(--primary-color), transparent)", width:"49%", pointerEvents:"none"}}></div>
+                        <div className="col-3" style={{position:"relative", top:"0.5rem"}}>
+                            <a target="_blank" href={expandedProject!=null?`https://github.com/H4rsh-prog/${projects[expandedProject].repository}`:null}>
+                                <div id="project_details" style={{opacity:0, postion:"fixed", width:"100%", height:"100%", backgroundSize:"contain", backgroundRepeat:"no-repeat", backgroundPosition:"right"}}></div>
+                            </a>
+                        </div>
+                        {pendingCardFetch?<div className="col w-100 h-100 me-2" style={{backgroundSize:"contain", backgroundRepeat:"no-repeat", backgroundPosition:"center", backgroundImage:loading_gif}}/>:null}
+                        <div className="col w-100 h-100 me-2" ref={ref.zoomed_details} id="project_details_zoomed" style={{borderRadius:"2rem", opacity:pendingCardFetch?0:1, backgroundSize:"contain", backgroundRepeat:"no-repeat", backgroundPosition:"center"}}/>
                     </div>
-                    <div className="col-3" style={{position:"relative", top:"0.5rem"}}>
-                        <a target="_blank" href={expandedProject!=null?`https://github.com/H4rsh-prog/${projects[expandedProject].repository}`:null}>
-                            <div id="project_details" style={{opacity:0, postion:"fixed", width:"100%", height:"100%", backgroundSize:"contain", backgroundRepeat:"no-repeat", backgroundPosition:"right"}}></div>
-                        </a>
+                :
+                    <div className="row mx-auto" style={{overflow:"hidden", height:"70%", top:"8rem", position:"relative"}}>
+                        <div className="col ms-3 px-4" style={{overflowY:"scroll", height:"100%", zIndex:1}}>
+                            { expandedProject == null ?
+                                <div className="row g-2 my-2">
+                                    {projects.map((project, index) => (
+                                    <div key={index} className="card mb-2 project_item"
+                                        onClick={
+                                            ()=> {
+                                                if(expandedProject == index) {
+                                                    setExpandedProject(null);
+                                                } else {
+                                                    setExpandedProject(index);
+                                                }
+                                            }
+                                        }>
+                                        <div className="card-body" >
+                                            <h5 className="card-title project-item-name">{pendingCardFetch && expandedProject==index?"Loading..":project.name}</h5>
+                                        </div>
+                                    </div>
+                                ))}
+                                </div>
+                            : 
+                                <div className="col h-100" style={{position:"relative", top:"0.5rem"}}>
+                                    <a target="_blank" href={`https://github.com/H4rsh-prog/${projects[expandedProject].repository}`}>
+                                        <div id="project_details" style={{width:"100%", height:"80%", transform:"scaleX(1.1)", backgroundSize:"contain", backgroundRepeat:"no-repeat", backgroundPosition:"right"}}></div>
+                                    </a>
+                                    <div style={{backgroundColor:"var(--secondary-color)", border:"0.5rem solid var(--accent-color)", borderRadius:"2rem", color:"var(--accent-color)"}}>BACK</div>
+                                </div>
+                            }
+                            <div className="position-absolute h-25 bottom-0 start-0 ms-4" style={{background:"linear-gradient(transparent, var(--primary-color))", width:"100%", pointerEvents:"none"}}></div>
+                            <div className="position-absolute h-25 top-0 start-0 ms-4" style={{background:"linear-gradient(var(--primary-color), transparent)", width:"100%", pointerEvents:"none"}}></div>
+                        </div>
                     </div>
-                    {pendingCardFetch?<div className="col w-100 h-100 me-2" style={{backgroundSize:"contain", backgroundRepeat:"no-repeat", backgroundPosition:"center", backgroundImage:loading_gif}}/>:null}
-                    <div className="col w-100 h-100 me-2" ref={ref.zoomed_details} id="project_details_zoomed" style={{borderRadius:"2rem", opacity:pendingCardFetch?0:1, backgroundSize:"contain", backgroundRepeat:"no-repeat", backgroundPosition:"center"}}/>
-                </div>
+                }
             </div>
         </div>
     );
